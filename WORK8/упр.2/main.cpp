7103edd09742c617fc665153f3e111c2e60e6bec
dezertod11 <79857528+dezertod11@users.noreply.github.com>
2021-10-29 17:14:34 +0300

    Add files via upload

--- WORK8/упр.2/main.cpp
+++ WORK8/упр.2/main.cpp
@@ -1,66 +1,67 @@
-#include <iostream>
-#include <windows.h>
+#include<iostream>
+#include<exception>
+#include<stdexcept>
+#include <math.h>
 
-double calculate(int a, int b){
-    if(b == 0){
-        throw std::invalid_argument("Знаменатель 0");
+
+double speed(double path, double time)
+{
+    if (time == 0)
+    {
+        throw std::domain_error("Cannot be divided by 0");
     }
-
-    return (a*1.0/b);
+    return path / time;
 }
 
-class Array{
-private:
-    int size;
-    int *arr;
-public:
-    Array(int sz): size(sz){arr = new int[sz];}
-    int& operator[](int index){
-        if(index > size){
-            throw std::range_error("ошибка overflow");
-        }
+double square(double a, double b, double c)
+{
+    if (a + b < c || a + c < b || c + b < a)
+    {
+        throw std::logic_error("This isn't a triangle");
+    }
+    if (a * b * c <= 0)
+    {
+        throw std::invalid_argument("Sides must be greater than 0");
+    }
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
 
-        return arr[index];
-    };
-};
 
-int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    int numerator, denominator;
-    std::cout << "Введите числитель:" << std::endl;
-    std::cin >> numerator;
-    std::cout << "Введите знаменатель:" << std::endl;
-    std::cin >> denominator;
 
-    try{
-        std::cout << calculate(numerator, denominator) << std::endl;
-    } catch(std::invalid_argument& e){
-        std::cout << e.what() << std::endl;
+
+int main()
+{
+    try
+    {
+        auto sp = speed(22, 0);
+    }
+    catch (const std::domain_error& e)
+    {
+        std::cerr << e.what() << '\n';
+        std::abort();
     }
 
-    int size, index;
-    std::cout << "Введите длину массива: " << std::endl;
-    std::cin >> size;
-    Array arr(size);
-    std::cout << "Введите индекс элемента, которые преобразуем в 1: " << std::endl;
-    std::cin >> index;
-    try {
-        arr[index] = 1;
-        std::cout << arr[index] << std::endl;
-        std::cout << "Нет проблем" << std::endl;
-    } catch (std::range_error& e){
-        std::cout << e.what() << std::endl;
+
+    try
+    {
+        auto sq = square(1.0, 2.0, 3.0);
+    }
+    catch (const std::logic_error& e)
+    {
+        std::cerr << e.what() << '\n';
+        std::abort();
+    }
+
+    try
+    {
+        auto sq = square(0, 0, 0);
+    }
+    catch (const std::invalid_argument& e)
+    {
+        std::cerr << e.what() << '\n';
+        std::abort();
     }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
